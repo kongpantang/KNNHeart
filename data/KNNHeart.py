12a4012dc0072f7b20e60efc1c88fb78c9c5a114
kongpantang <109472129+kongpantang@users.noreply.github.com>
2025-04-09 14:16:04 +0700

    .commit

---
+++ data/KNNHeart.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.header('การจำแนกข้อมูลด้วยเทคนิค ML(KNNHeart)')
